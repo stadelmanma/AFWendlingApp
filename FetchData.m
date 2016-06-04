5fce6aabf4773d526c6025590d44a85a60f13f20
stadelmanma <stadelmanma@gmail.com>
2016-06-04 18:22:58 -0400

    .

--- FetchData.m
+++ FetchData.m
@@ -112,7 +112,7 @@ static NSString *catKey = @"productCategories";
     NSString *postLength = [NSString stringWithFormat:@"%d",(int)[postData length]];
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
     //
-    NSString *afwUrl = @"http://afwendling.com/search_v2/product_search.php";
+    NSString *afwUrl = @"http://www.afwendling.com/search_v2/product_search.php";
     //NSString *localUrl = @"http://127.0.0.1/~Matt_Stadelman/afw-product_search/product-search/product_search.php";
     //
     [request setURL:[NSURL URLWithString:afwUrl]];
